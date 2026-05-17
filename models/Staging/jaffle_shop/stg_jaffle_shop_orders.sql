9d2c4b3c092e4116080a994211f9af79bd160bb3
mmabdullah05@gmail.com <mmabdullah05@gmail.com>
2026-05-17 19:06:23 +0000

    Freshness added and voided.

--- models/Staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/Staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -6,6 +6,7 @@
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,
+        CURRENT_TIMESTAMP as etl_loaded_at 
 
     from {{ source('jaffle_shop', 'orders') }}
