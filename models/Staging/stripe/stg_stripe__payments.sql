3a6150a997603e66a06705a585c054425c361cea
mmabdullah05@gmail.com <mmabdullah05@gmail.com>
2026-05-20 16:21:11 +0000

    converted the payment amount to cents.

--- models/Staging/stripe/stg_stripe__payments.sql
+++ models/Staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,7 @@
   id as payment_id,
   orderid as order_id,
   paymentmethod as payment_method,
-  amount as payment_amount,
+  amount / 100  as payment_amount,
   created as purchase_date
  from {{ source('stripe', 'payments') }}
   where status = 'success'
